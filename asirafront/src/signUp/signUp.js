dcee4634f18288faea678253056db2be757f7b35
darkshadow9799 <jainaayush99.aj@gmail.com>
2021-10-25 22:05:50 +0530

    Frontend start

---
+++ asirafront/src/signUp/signUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signUp.css';
+
+const SignUp = () => {
+    return (
+        <div className="background">
+            <h1>ASIRA</h1>
+        </div>
+    );
+}
+
+export default SignUp;
