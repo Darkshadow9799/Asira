29af9c83fbe2647bc7b3778ac056446030564470
darkshadow9799 <jainaayush99.aj@gmail.com>
2021-10-30 18:47:33 +0530

    post method for signIn added

--- asirafront/src/signIn/signIn.js
+++ asirafront/src/signIn/signIn.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import './signIn.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AiOutlineLock } from 'react-icons/ai'
+import { AiOutlineLock } from 'react-icons/ai';
+import axios from "axios";
 
 const SignIn = () => {
-        const [name, setName] = useState();
-        const [password, setPassword] = useState();
+      const [name, setName] = useState();
+      const [password, setPassword] = useState();
+      const endpoint = "http://localhost:8080/api/authenticate";
     
       const handleChangeName= (event) => {
         setName(event.target.value);
@@ -16,8 +18,16 @@ const SignIn = () => {
       }
     
       const handleSubmit = (event) => {
-        alert('A name was submitted: ' + name);
         console.log(password);
+        var data = {
+          "username": name,
+          "password": password,
+          "rememberMe":false
+        }
+        axios.post(endpoint, data).then(res => {
+          // console.log(res);
+          localStorage.setItem("authorization", res.data.id_token);
+        });
         event.preventDefault();
       }
     
@@ -27,7 +37,7 @@ const SignIn = () => {
             <form onSubmit={handleSubmit}>
                 <h2 className="visually-hidden">Login Form</h2>
                 <div className="illustration"><AiOutlineLock color={'#089CC8 '} height="250px" width="250px" /></div>
-                <div className="mb-3"><input type="email" className="form-control" placeholder="email" value={name || ''} onChange={handleChangeName} /></div>
+                <div className="mb-3"><input type="text" className="form-control" placeholder="email" value={name || ''} onChange={handleChangeName} /></div>
                 <div className="mb-3"><input type="password" className="form-control" placeholder="Password" value={password || ''} onChange={handleChangePassword}/></div>
                 <div className="mb-3"><button  className="btn btn-primary d-block w-100" type="submit">Log In</button></div>
             </form>
